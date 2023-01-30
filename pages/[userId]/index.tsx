da0163bab3f3c89c2c89866e92979aaea8a5c11a
oswhyteknits <orincywhyte@gmail.com>
2023-01-30 16:09:43 -0500

    new logic to user pages

--- pages/[userId]/index.tsx
+++ pages/[userId]/index.tsx
@@ -1,52 +1,138 @@
+import Head from "next/head";
+import { useSWRConfig } from "swr";
+
+import { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { PrismaClient } from "@prisma/client";
 import Input from "../../components/input/Input";
 import Output from "../../components/output/Output";
 import { ThemeContext } from "../../context/AppContext";
-import { useContext } from "react";
-import { useRouter } from "next/router";
-import { useSession } from "next-auth/react";
-import { useSWRConfig } from "swr";
+import { useSession, getSession } from "next-auth/react";
+import { InferGetServerSidePropsType } from "next";
 
-export default function User(props: any) {
-  const theme = useContext(ThemeContext);
-
+export default function User({
+      user,
+    }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // theme
+  const { theme } = useContext(ThemeContext);
+  // session data
   const { data: session } = useSession();
   // router
   const router = useRouter();
+  console.log(router);
+  const username = user?.id;
   const { userID } = router.query;
+  useEffect(() => {
+    router.push({
+      query: {
+        userID: username,
+      },
+    });
+  }, [username]);
   const { mutate } = useSWRConfig();
+
+  // data from getServerSideProps
 
   const createMessage = async (id?: string, message?: string) => {
     if (session) {
-      {
-        try {
-          const body = { id, message, favorite: 0 };
-          const response = await fetch(`/api/messages`, {
-            method: "POST",
-            body: JSON.stringify(body),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-          const data = await response.json();
-          mutate(`/api/messages/${userID}`);
-        } catch (error) {
-          return error;
-        }
+      try {
+        const url = `/api/messages/`;
+        const body = { id, message, favorite: 0 };
+        const options = {
+          method: "POST",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await fetch(url, options);
+        const data = await response.json();
+        mutate(`${url}${userID}`);
+      } catch (error) {
+        return error;
       }
     }
   };
-
+  const updateMessage = async (id: number, message: string) => {
+    if (session) {
+      try {
+        const url = `/api/messages/${userID}`;
+        const body = { id, message };
+        const options = {
+          method: "PUT",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await fetch(url, options);
+        const data = await response.json();
+        mutate(`${url}${userID}`);
+        return data;
+      } catch (error) {
+        return error;
+      }
+    }
+  };
+  const deleteMessage = async (id: number) => {
+    if (session) {
+      try {
+        const url = `/api/messages/${userID}`;
+        const body = { id };
+        const options = {
+          method: "DELETE",
+          body: JSON.stringify(body),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+        const response = await fetch(url, options);
+        const data = await response.json();
+        mutate(`${url}`);
+        return data;
+      } catch (error) {
+        return error;
+      }
+    }
+  };
   return (
     <section
       className={`
       ${theme === "light" ? "bg-white" : "bg-clrWhite"} 
-      h-screen flex m-auto w-[calc(100%-20px)] max-w-[800px] 
+      h-screen flex m-auto w-[calc(100%-10px)] max-w-[800px] 
       `}
     >
-      <div className="w-full grid md:grid-cols-6 grid-cols-1">
+      <Head>
+        <title>Share Your Thoughts</title>
+        <meta
+          name="description"
+          content="Messaging app to share your thoughts."
+        />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="w-full grid md:grid-cols-6 grid-cols-1 grid-rows-5">
         <Input createMessage={createMessage} />
-        <Output />
+        <Output deleteMessage={deleteMessage} updateMessage={updateMessage} />
       </div>
     </section>
   );
 }
+
+export const getServerSideProps = async ({ req }) => {
+  const prisma = new PrismaClient();
+  const session = await getSession({ req });
+  if (!session) {
+    return {
+      props: { user: [] },
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+  const user = await prisma.user.findFirst({
+    where: {
+      id: session?.userId,
+    },
+  });
+  return { props: { user } };
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,68 @@
 import Head from "next/head";
+import Link from "next/link";
+import clsx from "clsx";
 import { useContext } from "react";
+import { useRouter } from "next/router";
 import { ThemeContext } from "../context/AppContext";
 import { PrimaryButton } from "../components/button/Button";
-import { useSession, signIn, signOut } from "next-auth/react";
-import Link from "next/link";
+import { useSession, getSession, signIn, signOut } from "next-auth/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
+import { PrismaClient } from "@prisma/client";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home(props: any) {
+export default function Home({}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { data: session, status } = useSession();
-  const  theme  = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
+
+  // routes
+  const router = useRouter();
+  const { userID } = router.query;
   return (
-    <div
-      className={`
-        ${theme === "light" ? " bg-clrWhite" : "bg-clrGrey"}
-      `}
-    >
+    <div className={clsx(theme === "light" ? " bg-clrWhite" : "bg-clrGrey")}>
       <Head>
         <title>Share Your Thoughts</title>
         <meta
           name="description"
           content="Messaging app to share your thoughts."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className="flex m-auto h-screen w-[calc(100%-20px)] max-w-[800px] bg-blue-200">
-        <div className="w-23">
-          <div id="Hero" className="m-20 flex flex-col items-start gap-5">
+      <main
+        className={clsx(
+          theme === "light" ? "bg-clrWhite" : "bg-clrGrey",
+          "flex m-auto h-screen w-[calc(100%-10px)] max-w-[800px] p-3 md:p-6 "
+        )}
+      >
+        <div className="">
+          <div id="Hero" className="p-2 flex flex-col items-start gap-5">
             <span className="uppercase font-bold text-5xl">
               A home for
               <br />
               your thoughts
             </span>
-            <p>Share your thoughts today, and edit them tomorrow</p>
+
             {status === "authenticated" ? (
-              <div className="flex items-center gap-3">
-                <Link href="/api/auth/signout">
+              <div className="p-2">
+                <Link href={`/${userID}/profile`}>
                   <PrimaryButton
                     onClick={(e) => {
                       e.preventDefault();
-                      signOut();
                     }}
-                    ButtonValue="Sign Out"
+                    ButtonValue="Visit profile to get started "
                   ></PrimaryButton>
+                  <FontAwesomeIcon
+                    className="pl-2"
+                    icon={faArrowAltCircleRight}
+                  ></FontAwesomeIcon>
                 </Link>
-                <p className=" font-bold ">Signed in as {session.user?.name}</p>
               </div>
             ) : (
-              <div>
-                <p>Not signed in</p>
+              <div className="p-3">
+                <p className="font-bold pt-3">
+                  You are currently not signed in.
+                </p>
                 <Link href="/api/auth/signin">
                   <PrimaryButton
                     onClick={(e) => {
@@ -65,3 +80,14 @@ export default function Home(props: any) {
     </div>
   );
 }
+
+export const getServerSideProps = async ({ req }) => {
+  const prisma = new PrismaClient();
+  const session = await getSession({ req });
+  const user = await prisma.user.findFirst({
+    where: {
+      id: session?.userId,
+    },
+  });
+  return { props: { user } };
+};
